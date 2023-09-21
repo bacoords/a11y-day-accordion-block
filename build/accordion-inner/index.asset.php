4e04846f9dcf7439380dda61834c2eaf966eb86c
Brian Coords <bacoords@gmail.com>
2023-09-21 06:36:21 -0700

    adds toggle icon to the editor #6

--- build/accordion-inner/index.asset.php
+++ build/accordion-inner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '0bd832339e0c50b2bd20');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c38e64a8c47e2ca3e08e');
