961b7e45e6ce0f9b6324aab679c4f643c86f371a
Aurooba Ahmed <hey@aurooba.com>
2023-09-27 15:54:24 -0600

    update css classes

--- build/accordion-section/index.asset.php
+++ build/accordion-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b3cb68b86516d357d945');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7f78e05c01112a25eadc');
