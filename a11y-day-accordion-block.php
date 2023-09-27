3d7a53e9fccf5674f2c02184bc4e06e90a676912
Aurooba Ahmed <hey@aurooba.com>
2023-09-27 16:56:04 -0600

    update php file metadata

--- a11y-day-accordion-block.php
+++ a11y-day-accordion-block.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       WP Accessibility Day Accordion Block
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       An accessible accordion block built for WP Accessibility Day 2023.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Aurooba Ahmed & Brian Coords
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       accordion-block
