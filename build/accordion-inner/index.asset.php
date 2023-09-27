ccd3876d6c75b0716abf72ecf87cca9e99e7db22
Aurooba Ahmed <hey@aurooba.com>
2023-09-27 12:10:30 -0600

    match padding in front and editor

--- build/accordion-inner/index.asset.php
+++ build/accordion-inner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'dc358614b4c8706c40ab');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '27afa9340ee541c076ec');
